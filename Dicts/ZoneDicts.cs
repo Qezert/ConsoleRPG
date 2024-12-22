f13a24b967182eea279f0428308de3f5959f4a2d
Qezert <MagBerKri@gmail.com>
2024-12-23 00:11:21 +0100

    feat: Add a list of monsters that may spawn in a given zone

--- Dicts/ZoneDicts.cs
+++ Dicts/ZoneDicts.cs
@@ -2,5 +2,8 @@ namespace ConsoleRPG;
 
 public class ZoneDicts
 {
-    
+    public static readonly Dictionary<int, List<string>> MonstersInZone = new Dictionary<int, List<string>>()
+    {
+        {1, ["goblin", "zombie", "rat"] }
+    };
 }
