7e581c3a9a2e8dc4b09ae366d66f9ef71e4b9ade
Qezert <MagBerKri@gmail.com>
2024-12-23 21:53:36 +0100

    chore: add comments to RunFight function in program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 bool RunFight(Monster m, Player p)
 {
+    // Executing combat
     while (m.Health > 0 && p.health > 0)
     {
         Console.WriteLine("> Press enter to attack!");
@@ -10,6 +11,7 @@ bool RunFight(Monster m, Player p)
         p.TakeDamage(m.Attack());
     }
 
+    // Player survived combat
     if (p.health > 0)
     {
         int exp = m.Die(p.health);
@@ -17,6 +19,7 @@ bool RunFight(Monster m, Player p)
         return true;
     }
 
+    // Player died
     Console.WriteLine("You were slain... try again!");
     return false;
 }
